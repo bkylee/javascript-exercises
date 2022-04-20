037a130aefeb56bb2a59ffc451d1d407d7b4d8ab
bkylee <brian.ky.lee@outlook.com>
2022-04-20 15:34:32 -0400

    tried completing sumAll.js

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,7 @@
-const sumAll = function() {
-
+const sumAll = function(thing, ...nums) {
+    let modArray = thing.filter(x => !nums.includes(x));
+    total = modArray.reduce((previousValue, currentValue) => {return previousValue + currentValue});
+    return total;
 };
 
 // Do not edit below this line
