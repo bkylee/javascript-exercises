b3b29ff594a6c70e1a51f3be30700c9aea2a5b1c
bkylee <brian.ky.lee@outlook.com>
2022-04-20 20:50:06 -0400

    made changes to new array function and tried to conver to an array and elements to numbers

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,8 +1,8 @@
 const sumAll = function(thing, ...nums) {
-    modArray = (thing,nums) => 
-    {newArray = [];
-        for (i of thing) {
-        for (j of nums) {
+    //create a function to make a new array that has the common elements between the array enetered in the parameter and the arguments passed
+    let newArray = (thing,nums) => 
+    {for (i= 0; i<thing.length; i++) {
+        for (j=0; i<nums.length; i++) {
             if (thing[i] === nums[j]){
                 newArray.push(thing[i]);
             }
@@ -10,10 +10,12 @@ const sumAll = function(thing, ...nums) {
     }
     return newArray; 
 }
-    let newArray = modArray(thing,nums);
-    total = modArray.reduce((previousValue, currentValue) => {return previousValue + currentValue});
+    newArray(thing, nums); 
+    total = Array.from(newArray);
+    total = total.map(Number);
+    total = total.reduce((previousValue, currentValue) => {return previousValue + currentValue});
     return total;
-});
+};
 
 // Do not edit below this line
 module.exports = sumAll;
