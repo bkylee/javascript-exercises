bb6c5bcb05ffa9ead1e845eb82035fc1267e4694
bkylee <brian.ky.lee@outlook.com>
2022-04-20 20:27:59 -0400

    Tried to create a function to make a new array that will have the common values between array and wanted values to be summed

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,8 +1,19 @@
 const sumAll = function(thing, ...nums) {
-    let modArray = thing.filter(x => !nums.includes(x));
+    modArray = (thing,nums) => 
+    {newArray = [];
+        for (i of thing) {
+        for (j of nums) {
+            if (thing[i] === nums[j]){
+                newArray.push(thing[i]);
+            }
+        }
+    }
+    return newArray; 
+}
+    let newArray = modArray(thing,nums);
     total = modArray.reduce((previousValue, currentValue) => {return previousValue + currentValue});
     return total;
-};
+});
 
 // Do not edit below this line
 module.exports = sumAll;
