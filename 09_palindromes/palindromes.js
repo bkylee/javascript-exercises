e10065673bb524db51aa0ca2f8722a3cee675b01
bkylee <brian.ky.lee@outlook.com>
2022-04-22 16:47:29 -0400

    Completed creating opposite of original word

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,19 @@
-const palindromes = function () {
-
+const palindromes = function (theThing) {
+    //var for opposite of theThing
+    oppThing ="";
+    //array for original argument 
+    thing = Array.from(theThing);
+    //length of original argument 
+    end = thing.length;
+    //loop to create opposite of original array 
+    for (i = 0; i <= end; i ++) {
+        opThing = thing.pop();
+    }
+    return opThing;
 };
+
+
+palindromes("hello");
 
 // Do not edit below this line
 module.exports = palindromes;
