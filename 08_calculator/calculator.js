afcf68fa8291658803eab8e45ef59ab20b267344
bkylee <brian.ky.lee@outlook.com>
2022-04-22 15:13:32 -0400

    Completed basis of each function

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,38 @@
-const add = function() {
-	
+const add = function(x,y) {
+  let sum = x + y;
+  return sum;
 };
 
-const subtract = function() {
-	
+const subtract = function(x,y) {
+	result = x-y;
+  return result;
 };
 
-const sum = function() {
-	
+const sum = function(...numbers) {
+  if (numbers = true) {
+  let numArray = numbers;
+  return numArray.reduce((x,y) => {return x + y});
+  }
+  else return 0;
 };
 
-const multiply = function() {
-
+const multiply = function(x,y) {
+  total = x*y;
+  return total;
 };
 
-const power = function() {
-	
+const power = function(x,y) {
+	total = x**y;
+  return total;
 };
 
-const factorial = function() {
-	
+const factorial = function(x) {
+	if (x = 0) {
+    return 1;
+  }
+    else {
+      return (x * factorial-1);
+  }
 };
 
 // Do not edit below this line
