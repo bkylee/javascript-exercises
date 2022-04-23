dfdb9cd7357195257d0c7ed79622a24d9687dfe7
bkylee <brian.ky.lee@outlook.com>
2022-04-23 18:12:35 -0400

    Was iterating backwards on opThing even though it's already backwards. Corrected iteration and made counter to confirm if true or not. Working on ignoring punctuation

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,16 +1,32 @@
 const palindromes = function (theThing) {
+    const punctation = '!"#$%&\'()*+,-./:;<=>?@[\\]^_`{|}~' ;
     opThing = [];
     //array for original argument 
-    thing = Array.from(theThing);
+    thing = theThing.split('').filter((letter) => {return punctation.indexOf(letter) === -1;});
     //length of original argument 
-    end = thing.length;
+    let end = thing.length  ;
     //loop to create opposite of original array 
+    let counter = 0;
+
+
     for (i = 0; i < end; i ++) {
         opThing.push(thing.pop());
     }
     thing = Array.from(theThing);
-    result = opThing.every((element) => {return thing.indexOf(element) !== -1})
-    return result;
+    
+    for (i=end -1; i >= 0; i --){
+        if (thing[i] === opThing[i]) {
+            counter = counter +1;
+        }
+    }
+    if (counter === end) {
+        return true;
+    }
+    else return false;
+    //result = opThing.every((element) => {return thing.indexOf(element) !== -1})
+    //return result;
 };
+
+palindromes('ZZZZ car, a man, a maracaz.');
 // Do not edit below this line
 module.exports = palindromes;
