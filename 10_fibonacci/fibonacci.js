5f5b33341910a52317c9d54a084fa0c77c24b3f0
bkylee <brian.ky.lee@outlook.com>
2022-04-24 19:01:35 -0400

    Completed exercise

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,18 @@
-const fibonacci = function() {
-
+const fibonacci = function(x) {
+    x = Number(x);
+    if (x === 0) {
+        return 0;
+    }
+    else if (x < 0) {
+        return "OOPS";
+    }
+    else if (x === 1){
+        return 1;
+    }
+    else result = fibonacci(x-1) + fibonacci(x-2);
+    return result; 
 };
+fibonacci(4);
 
 // Do not edit below this line
 module.exports = fibonacci;
