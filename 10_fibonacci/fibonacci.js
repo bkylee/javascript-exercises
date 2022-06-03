c5bf0921deba911cdbc315d0446d52659866461a
bkylee <brian.ky.lee@outlook.com>
2022-06-03 14:16:41 -0400

    Started by doing if statements for x = 1,0 and onward

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,10 @@
-const fibonacci = function() {
-
+const fibonacci = function(n) {
+    if (x === 1 || x === 0 ) {
+        return 1
+    }
+    else {
+        return fibonacci(x-1) + fibonacci(x-2);
+    }
 };
 
 // Do not edit below this line
